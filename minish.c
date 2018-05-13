4cd4831d0cfff853d740bc723eb94c3cdb5ab28a
PresentLee <darkmagicsun@gmail.com>
2018-05-13 22:17:48 +0900

    developing

--- minish.c
+++ minish.c
@@ -11,6 +11,8 @@
    5. print the result (at terminal)
 */
 
+#define buf 1024
+
 int main(int argc, char **argv, char **envp)
 {
 
@@ -18,18 +20,33 @@ int main(int argc, char **argv, char **envp)
 	struct dirent *dir_entry;
 	int fd;
 	pid_t pid;
+	char *path;
 
-	dir_info = opendir("/bin");
+//	printf("%s \n",getenv("SHELL"));
+//	printf("%s \n", argv[0]);
+	printf("%s \n", argv[1]);
+
+	if(-1 == putenv("PATH=/bin"))
+	{
+		printf("putenv failed \n");
+	}
+	path = getenv("PATH");
+//	printf("%s \n", path);
+	
+	dir_info = opendir(path);
+
 	if(NULL != dir_info)
 	{
 		while(dir_entry = readdir(dir_info))
 		{
-			if(strchr(dir_entry->d_name,argv[0]) != NULL)
+			if(strcmp(dir_entry->d_name,argv[1]) ==0)
 			{
+				printf("%s", dir_entry->d_name);
 				printf("%s", getenv(dir_entry->d_name));
-			}
-			closedir(dir_info);
+			} 
+		//	printf("%s \n", dir_entry->d_name);
 		}
+		closedir(dir_info);
 				
 	}
 
